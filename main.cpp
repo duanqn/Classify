f1b1e43c07e65c4694d494f2d9212eccccfbc0e1
Qingnan Duan <qingnan.duan@yitu-inc.com>
2018-05-29 15:24:04 +0800

    Comment out the debug flag. Prepare to integrate with the SA algorithm part.

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 #include <fstream>
 #include <string>
 
-#define DEBUG 1
+//#define DEBUG 1
 
 int Student::s_numSubject = 3;
 
@@ -40,8 +40,6 @@ int main(){
   // test output
   
   std::cout << setlocale(LC_ALL, NULL) << std::endl;
-  std::setlocale(LC_ALL, "Chinese_China.936");
-  std::cout << setlocale(LC_ALL, NULL) << std::endl;
   std::cout.imbue(std::locale());
   std::cout << u8"测试" << std::endl;
   std::ofstream outfile;
@@ -54,6 +52,29 @@ int main(){
   // read in student data
   // run SA
   // print results
+  std::string filename;
+  std::cout << "Please input filename: ";
+  std::cin >> filename;
+  
+  std::ifstream datafile;
+  datafile.open(filename, std::ios::in);
+  
+  int numStudents, numClasses, numSubjects;
+  datafile >> numStudents >> numClasses >> numSubjects;
+  std::cout << numStudents << " " << numClasses << std::endl;
+  
+  Solution *res = new Solution;
+  res->init(numClasses);
+  
+  Student::s_numSubject = numSubjects;
+  Student *s;
+  for(int i = 0; i < numStudents; ++i){
+    s = new Student();
+    datafile >> *s;
+    res->addStudentAndSetClass(s);
+    //std::cout << *s;
+  }
+  datafile.close();
   #endif
   return 0;
 }
