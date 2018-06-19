bfb8982aa93bec560f5c0ff82e6c56a83e708814
Qingnan Duan <duanqn@users.noreply.github.com>
2018-06-19 10:24:30 +0800

    Move definition of Student::s_numsubject to common, and define a set of constant values

--- common.h
+++ common.h
@@ -25,8 +25,20 @@ namespace duanqn{
 #define TEMP_DROP_RATE 0.97
 #define STOP_TEMP 0.001
 
-const int n_instance = 4;
+int Student::s_numSubject = 3;
 
+const int n_instance = 4;
+const char cg_in_delimiter = ',';  // delimiter for CSV
+const char cg_out_delimiter = ' ';
+const enum DataType cg_student_field_ordered[] = {
+  INFO_GB2312,         // 姓名
+  GENDER,       // 性别
+  INFO_UNIQUE,  // 学号
+  SCORE,        // 语文
+  SCORE,        // 数学
+  SCORE,        // 英语
+  DUMP          // 总分
+};
 
 std::string toUTF8String(const wchar_t * str, int len);
 std::string toUTF8String(const std::wstring& str);
