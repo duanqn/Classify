e78e447ec2169af5d4b26c6ddf9d51598b0611ce
Qingnan Duan <duanqn_own_1@yeah.net>
2018-10-29 16:09:33 -0400

    Move global variables to common.cpp to avoid multiple definition

--- common.h
+++ common.h
@@ -14,19 +14,11 @@
 #define STOP_TEMP 0.001
 
 
-const int n_instance = 8;
-const char cg_in_delimiter = ',';  // delimiter for CSV
-const char cg_out_delimiter = ' ';
-const enum DataType cg_student_field_ordered[] = {
-  INFO_GB2312,         // 姓名
-  GENDER,       // 性别
-  INFO_UNIQUE,  // 学号
-  SCORE,        // 语文
-  SCORE,        // 数学
-  SCORE,        // 英语
-  DUMP          // 总分
-};
-const unsigned cgu_nFields = sizeof(cg_student_field_ordered) / sizeof(DataType);
+extern const int n_instance;
+extern const char cg_in_delimiter;  // delimiter for CSV
+extern const char cg_out_delimiter;
+extern enum DataType cg_student_field_ordered[];
+extern const unsigned cgu_nFields;
 
 std::string toUTF8String(const wchar_t * str, int len);
 std::string toUTF8String(const std::wstring& str);
