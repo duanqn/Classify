7f0b411b348c4b1f0fbd3a12524134593fde6957
Qingnan Duan <duanqn_own_1@yeah.net>
2018-12-13 00:53:00 -0500

    Add a button in the window; set font; handle Windows versions

--- Classify/Classify.cpp
+++ Classify/Classify.cpp
@@ -3,6 +3,8 @@
 
 #include "stdafx.h"
 #include "Classify.h"
+#include "fontHandler.h"
+#include <VersionHelpers.h>
 
 #define MAX_LOADSTRING 100
 
@@ -28,7 +30,11 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     // TODO: 在此放置代码。
 
     // 初始化全局字符串
+	const WCHAR *title = _T("分班工具");
+	wmemcpy_s(szTitle, MAX_LOADSTRING, title, wcslen(title));
     LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+	OutputDebugString(szTitle);
+	OutputDebugString(_T("\n"));
     LoadStringW(hInstance, IDC_CLASSIFY, szWindowClass, MAX_LOADSTRING);
     MyRegisterClass(hInstance);
 
@@ -39,6 +45,12 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     }
 
     HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_CLASSIFY));
+
+	if (!IsWindows8OrGreater())
+	{
+		MessageBox(NULL, _T("You need at least Windows 8"), _T("Version Not Supported"), MB_OK);
+		goto fail;
+	}
 
     MSG msg;
 
@@ -52,6 +64,8 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
         }
     }
 
+	OutputDebugString(_T("退出\n"));
+fail:
     return (int) msg.wParam;
 }
 
@@ -105,6 +119,9 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
       return FALSE;
    }
 
+   HWND hButton = CreateWindowW(_T("BUTTON"), _T("选择配置文件"), WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON, 20, 20, 100, 40, hWnd, nullptr, hInstance, nullptr);
+
+   SendMessage(hWnd, WM_SETFONT, reinterpret_cast<WPARAM>(nullptr), MAKELPARAM(0,0));
    ShowWindow(hWnd, nCmdShow);
    UpdateWindow(hWnd);
 
@@ -123,10 +140,50 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 //
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
+	static HFONT fontYaHei = nullptr;
     switch (message)
     {
+	case WM_SETFONT:
+		{
+			// set font of buttons
+			HDC context = GetDC(hWnd);
+			// Use pixel size
+			SetMapMode(context, MM_TEXT);
+			const int textPointSize = 10;
+			int fontHeight = -MulDiv(textPointSize, GetDeviceCaps(context, LOGPIXELSY), 72);
+			LOGFONTW logFont;
+			logFont.lfCharSet = GB2312_CHARSET;
+			logFont.lfFaceName[0] = L'\0';
+			logFont.lfPitchAndFamily = 0;
+
+			std::vector<LOGFONTW> allFonts;
+			EnumFontFamiliesExW(context, &logFont, enumFontFamilyCallback, LPARAM(&allFonts), 0);
+			LOGFONTW *ptrYaHei = nullptr;
+			for (auto it = allFonts.begin(); it != allFonts.end(); ++it) {
+				if (wcscmp(it->lfFaceName, L"微软雅黑") == 0) {
+					ptrYaHei = &(*it);
+					break;
+				}
+				else if (wcscmp(it->lfFaceName, L"Microsoft YaHei UI") == 0) {
+					ptrYaHei = &(*it);
+					break;
+				}
+			}
+			if (ptrYaHei != nullptr) {
+				ptrYaHei->lfHeight = fontHeight;
+				ptrYaHei->lfWidth = 0;
+				fontYaHei = CreateFontIndirectW(reinterpret_cast<LOGFONTW *>(ptrYaHei));
+				bool dont_care = EnumChildWindows(
+					hWnd,
+					SetChildWindowFont,
+					reinterpret_cast<LPARAM>(fontYaHei)
+				);
+			}
+		}
+		break;
     case WM_COMMAND:
         {
+			OutputDebugString(_T("WM_COMMAND\n"));
             int wmId = LOWORD(wParam);
             // 分析菜单选择: 
             switch (wmId)
@@ -136,6 +193,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
                 break;
             case IDM_EXIT:
                 DestroyWindow(hWnd);
+				OutputDebugString(_T("Destroy window\n"));
                 break;
             default:
                 return DefWindowProc(hWnd, message, wParam, lParam);
@@ -159,6 +217,9 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 		}
 		break;
     case WM_DESTROY:
+		if (fontYaHei != nullptr) {
+			DeleteObject(fontYaHei);
+		}
         PostQuitMessage(0);
         break;
     default:
